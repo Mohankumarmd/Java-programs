2f8166280e4cd2435163b3c3827955c3d3fdd971
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-05 09:32:21 +0530

    Added files

--- o1.java
+++ o1.java
@@ -90,3 +90,22 @@ public class LargestInArray {
         System.out.println("Largest Number: " + max);
     }
 }
+
+
+import java.util.Scanner;
+
+public class AddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
