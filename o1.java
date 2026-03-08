9c07635b4e0196a3a5d940478f2c4e1543a1ac2d
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-08 09:42:42 +0530

    Add

--- o1.java
+++ o1.java
@@ -267,3 +267,11 @@ public class Palindrome {
             System.out.println("Not Palindrome");
     }
 }
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
