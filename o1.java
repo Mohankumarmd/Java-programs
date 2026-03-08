9df2d5b5261b4a0545d6843bbb7913008e7de694
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-08 09:43:54 +0530

    Add

--- o1.java
+++ o1.java
@@ -289,3 +289,15 @@ public class ArraySum {
         System.out.println("Sum = " + sum);
     }
 }
+
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+
+        System.out.println("Addition = " + (a+b));
+        System.out.println("Subtraction = " + (a-b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Division = " + (a/b));
+    }
+}
