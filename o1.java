bb560591875d8368265025111f7b26589f53412a
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-08 09:43:18 +0530

    Add

--- o1.java
+++ o1.java
@@ -275,3 +275,17 @@ public class PrintNumbers {
         }
     }
 }
+
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20};
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
