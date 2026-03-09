769746c9fc38b50361b769dc5b3eb5917e24984a
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-09 23:23:39 +0530

    Add

--- o1.java
+++ o1.java
@@ -301,3 +301,14 @@ public class Calculator {
         System.out.println("Division = " + (a/b));
     }
 }
+
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=20; i++) {
+            if(i % 2 == 0)
+                System.out.println(i);
+        }
+    }
+}
