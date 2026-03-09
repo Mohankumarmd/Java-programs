758b31297c5b4e555879f460eea7fc3fb8390d2a
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-09 23:25:45 +0530

    Add

--- o1.java
+++ o1.java
@@ -325,3 +325,17 @@ public class StarPattern {
         }
     }
 }
+
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String str = "Java";
+        String rev = "";
+
+        for(int i=str.length()-1; i>=0; i--)
+            rev += str.charAt(i);
+
+        System.out.println(rev);
+    }
+}
