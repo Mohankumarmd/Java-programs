32e4803d77202d10bf8d8aa76e2c699af4a8285e
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-09 23:24:41 +0530

    Add

--- o1.java
+++ o1.java
@@ -312,3 +312,16 @@ public class EvenNumbers {
         }
     }
 }
+
+
+public class StarPattern {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=i; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
