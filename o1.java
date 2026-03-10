472a7a98752cbfa74cd165c08760d3b9df9aab7a
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-10 18:24:56 +0530

    Add

--- o1.java
+++ o1.java
@@ -353,3 +353,13 @@ public class Pattern2 {
 }
 
 
+public class Pattern3 {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
