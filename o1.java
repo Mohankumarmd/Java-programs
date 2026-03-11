9a12f1b1da3eae209ae0f734e1b29b33a702fd57
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-11 21:53:02 +0530

    Add

--- o1.java
+++ o1.java
@@ -405,3 +405,16 @@ public class LinearSearch {
         }
     }
 }
+
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};
+
+        int index = Arrays.binarySearch(arr,30);
+
+        System.out.println("Index: " + index);
+    }
+}
