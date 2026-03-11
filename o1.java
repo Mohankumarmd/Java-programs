708bda5127c69fc21cc5097249c16e92d5707850
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-11 21:52:08 +0530

    Add

--- o1.java
+++ o1.java
@@ -376,3 +376,17 @@ public class SwapNumbers {
         System.out.println("a=" + a + " b=" + b);
     }
 }
+
+
+import java.util.Arrays;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int arr[] = {5,2,8,1};
+
+        Arrays.sort(arr);
+
+        for(int i: arr)
+            System.out.print(i + " ");
+    }
+}
