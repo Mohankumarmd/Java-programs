6b2f41a3d66030750ce042175d233ff68e9d7560
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-12 23:30:21 +0530

    Add

--- o1.java
+++ o1.java
@@ -431,3 +431,15 @@ public class RandomNumber {
     }
 }
 
+
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        int number = rand.nextInt(100); // 0 to 99
+        System.out.println("Random Number: " + number);
+    }
+}
