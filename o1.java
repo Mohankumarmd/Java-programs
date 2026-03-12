5f8e5231f5c823bc82dd9b8b01f2172b7fa0c4b0
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-12 23:29:50 +0530

    Add

--- o1.java
+++ o1.java
@@ -418,3 +418,16 @@ public class BinarySearch {
         System.out.println("Index: " + index);
     }
 }
+
+
+import java.util.Random;
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        Random r = new Random();
+
+        int num = r.nextInt(100);
+        System.out.println(num);
+    }
+}
+
