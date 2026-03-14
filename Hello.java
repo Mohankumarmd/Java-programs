d0c2b2304c8e4264fb3d30ce8fe38830fdf9a416
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-14 21:43:43 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -497,3 +497,14 @@ public class Main {
 
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str="Java is easy";
+        String words[]=str.split(" ");
+
+        System.out.println(words.length);
+
+    }
+}
