5efbf77c0977b3c264bae56528ced001d6150b1b
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-03-17 23:48:57 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -559,3 +559,15 @@ public class Vowels {
         System.out.println(count);
     }
 }
+
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
