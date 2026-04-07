035ad9e961bd581a0ebeb59ad8922180bfa47f88
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-04-07 21:00:40 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -666,3 +666,12 @@ public class Variables {
 	}
 
 }
+
+
+public class Example {
+    public static void main(String[] args) {
+        int a = 10;
+        double b = a;
+        System.out.println(b);
+    }
+}
