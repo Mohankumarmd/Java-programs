9aaa353dc9826f196c8896bcf5e2ea27f7840308
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-04-15 18:29:26 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -822,3 +822,23 @@ public class Demo {
         System.out.println(s1.compareTo(S2));
     }
 }
+
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int fact = 1;
+
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial is: " + fact);
+    }
+}
