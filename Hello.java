bfc888aa47a442da7d937cafd9ffc7bc22a1e470
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-04 09:37:54 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1098,3 +1098,5 @@ class Main {
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
