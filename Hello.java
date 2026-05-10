e149b0130b941451d80f0add998e47f62c4c092b
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-10 20:42:39 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1192,3 +1192,18 @@ public class Main {
         System.out.println(e.getName());
     }
 }
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
