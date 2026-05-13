c7804dfc33fefa6cdbc423b9dd0cb801082d3a7e
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-13 16:27:32 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1264,4 +1264,8 @@ public class Main {
     public static void main(String[] args) {
 
         int number = 7;
+            if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
 
