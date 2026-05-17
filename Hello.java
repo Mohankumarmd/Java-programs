44fde3a90c7bc06c28f5755db92c75eed290813b
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-17 11:59:31 +0530

    Addd

--- Hello.java
+++ Hello.java
@@ -1285,7 +1285,7 @@ public class Main {
     {
          System.out.println("Java");
     }
-//     public static void main(String[] args) {
+    public static void main(String[] args) {
 //         System.out.println("Python");
 //     }
 // }
