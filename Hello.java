17519812ec23592f4248dc53c340a8447bb5ba6c
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-19 20:37:21 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1304,8 +1304,8 @@ public class Main {
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
-//         int[] arr = new int[n];
+        int n = sc.nextInt();
+        int[] arr = new int[n];
         
 //         for (int i = 0; i < n; i++) {
 //             arr[i] = sc.nextInt();
