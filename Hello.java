66a56376f04621cc7f8cf0a7753c0925d084728a
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-19 20:36:59 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1299,3 +1299,21 @@ public class Main {
         a.sound(); 
     }
 }  
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int[] arr = new int[n];
+        
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//             for (int i = 0; i < n / 2; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+
+//         sc.close();
+//     }
+// }
