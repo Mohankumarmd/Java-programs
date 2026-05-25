ce4ff28b95d30c99df0fdf208b4e10297ac788ec
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-05-25 13:00:23 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1321,7 +1321,7 @@ public class Main {
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-//         System.out.println(i);
-//         }
+        System.out.println(i);
+        }
 //     }
 // }
