14bd519d2ba1ad8b79428614918f4a3f059b27bf
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-06-16 10:13:58 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1394,3 +1394,28 @@ class Main {
         }
     }
 }
+
+
+class MathOperation {
+//     int add (int a, int b) {
+//         return a + b;
+//     }
+
+//     int add (int a, int b, int c) {
+//         return a + b + c;
+//     }
+
+//     double add (double a, double b) {
+//         return a + b;
+//     }
+// }
+
+
+// public class Main {
+// public static void main(String[] args) {
+//     MathOperation obj = new MathOperation();
+//     System.out.println(obj.add(10, 20));
+//     System.out.println(obj.add(10, 20, 30));
+//     System.out.println(obj.add(5.5, 5.5));
+// }
+// }
