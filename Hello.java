4c2f2600b5632073dcd648a640921f9f8b61df56
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-06-27 18:40:19 +0530

    .

--- Hello.java
+++ Hello.java
@@ -1412,7 +1412,7 @@ class MathOperation {
 
 
 public class Main {
-// public static void main(String[] args) {
+public static void main(String[] args) {
 //     MathOperation obj = new MathOperation();
 //     System.out.println(obj.add(10, 20));
 //     System.out.println(obj.add(10, 20, 30));
