0916c9f4b0c98482076df2399a1de7192c47fd95
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-07-03 12:01:49 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1415,7 +1415,7 @@ public class Main {
 public static void main(String[] args) {
     MathOperation obj = new MathOperation();
     System.out.println(obj.add(10, 20));
-//     System.out.println(obj.add(10, 20, 30));
+    System.out.println(obj.add(10, 20, 30));
 //     System.out.println(obj.add(5.5, 5.5));
 // }
 // }
