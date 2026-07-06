fa6a10fad88ac487cd8910fa1eb0d45b688c0598
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-07-06 10:10:36 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1417,5 +1417,25 @@ public static void main(String[] args) {
     System.out.println(obj.add(10, 20));
     System.out.println(obj.add(10, 20, 30));
     System.out.println(obj.add(5.5, 5.5));
-// }
+}
+}
+
+
+
+
+public class Factorial {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.print("Enter a number: ");
+//         int num = sc.nextInt();
+
+//         int fact = 1;
+
+//         for (int i = 1; i <= num; i++) {
+//             fact = fact * i;
+//         }
+
+//         System.out.println("Factorial is: " + fact);
+//     }
 // }
