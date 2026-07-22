8f1c9c1280affc2bc2ceee75195e4f69b39d2de7
Mohan kumar M D <mohankumarmd410@gmail.com>
2026-07-22 14:21:13 +0530

    Add

--- Hello.java
+++ Hello.java
@@ -1433,7 +1433,7 @@ public class Factorial {
         int fact = 1;
 
         for (int i = 1; i <= num; i++) {
-//             fact = fact * i;
+            fact = fact * i;
 //         }
 
 //         System.out.println("Factorial is: " + fact);
